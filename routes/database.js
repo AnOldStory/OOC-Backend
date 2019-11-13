4de3122a1712f10665728b992ed212f3439150ca
MORTY <bearnt0306@gmail.com>
2019-11-13 13:34:42 +0900

    Added Query Function
    bENefICiUm LAbOra

--- routes/database.js
+++ routes/database.js
@@ -19,3 +19,202 @@ exports.getCinemaById = (id, callback) => {
       return callback(err, false);
     });
 };
+
+exports.getSchedules = (callback) => {
+  Models.Schedule.findAll({
+    subQuery: false,
+    include: [{
+      model: Models.Movie, as: 'movieIdSchedule'
+    }, {
+      model: Models.ShowRoom, as: 'cineIdRoomSche'
+    }]
+  }).then(result => {
+    console.log("getting schedules...");
+    return callback(null, result);
+  }).catch(err => {
+    console.log("error");
+    console.log(err);
+    return callback(err, false);
+  });
+};
+
+exports.getSchedulesbyMovie = (id, callback) => {
+  Models.Schedule.findAll({
+    subQuery: false,
+    where: {
+      [Op.and]: [{movieId: id}]
+    },
+    include: [{
+      model: Models.Movie, as: 'movieIdSchedule'
+    }, {
+      model: Models.ShowRoom, as: 'cineIdRoomSche'
+    }]
+  }).then(result => {
+    console.log("getting schedules by movie...");
+    return callback(null, result);
+  }).catch(err => {
+    console.log("error");
+    console.log(err);
+    return callback(err, false);
+  });
+};
+
+exports.getSchedulesbyCinema = (id, callback) => {
+  Models.Schedule.findAll({
+    subQuery: false,
+    where: {
+      [Op.and]: [{cinemaId: id}]
+    },
+    include: [{
+      model: Models.Movie, as: 'movieIdSchedule'
+    }, {
+      model: Models.ShowRoom, as: 'cineIdRoomSche'
+    }]
+  }).then(result => {
+    console.log("getting schedules by cinema...");
+    return callback(null, result);
+  }).catch(err => {
+    console.log("error");
+    console.log(err);
+    return callback(err, false);
+  });
+};
+
+exports.getSchedulesbyDate = (id, callback) => {
+  Models.Schedule.findAll({
+    subQuery: false,
+    where: {
+      [Op.and]: [{screeningDate: id}]
+    },
+    include: [{
+      model: Models.Movie, as: 'movieIdSchedule'
+    }, {
+      model: Models.ShowRoom, as: 'cineIdRoomSche'
+    }]
+  }).then(result => {
+    console.log("getting schedules by date...");
+    return callback(null, result);
+  }).catch(err => {
+    console.log("error");
+    console.log(err);
+    return callback(err, false);
+  });
+};
+
+
+exports.getSchedulesbyCinemaMovie = (cinema, movie, callback) => {
+  Models.Schedule.findAll({
+    subQuery: false,
+    where: {
+      [Op.and]: [{cinemaId: cinema}, {movieId: movie}]
+    },
+    include: [{
+      model: Models.Movie, as: 'movieIdSchedule'
+    }, {
+      model: Models.ShowRoom, as: 'cineIdRoomSche'
+    }]
+  }).then(result => {
+    console.log("getting schedules by cinema and movie...");
+    return callback(null, result);
+  }).catch(err => {
+    console.log("error");
+    console.log(err);
+    return callback(err, false);
+  });
+};
+
+exports.getSchedulesbyCinemaDate = (cinema, date, callback) => {
+  Models.Schedule.findAll({
+    subQuery: false,
+    where: {
+      [Op.and]: [{cinemaId: cinema}, {screeningDate: date}]
+    },
+    include: [{
+      model: Models.Movie, as: 'movieIdSchedule'
+    }, {
+      model: Models.ShowRoom, as: 'cineIdRoomSche'
+    }]
+  }).then(result => {
+    console.log("getting schedules by cinema and movie...");
+    return callback(null, result);
+  }).catch(err => {
+    console.log("error");
+    console.log(err);
+    return callback(err, false);
+  });
+};
+
+exports.getSchedulesbyMovieDate = (movie, date, callback) => {
+  Models.Schedule.findAll({
+    subQuery: false,
+    where: {
+      [Op.and]: [{movieId: movie}, {screeningDate: date}]
+    },
+    include: [{
+      model: Models.Movie, as: 'movieIdSchedule'
+    }, {
+      model: Models.ShowRoom, as: 'cineIdRoomSche'
+    }]
+  }).then(result => {
+    console.log("getting schedules by cinema and movie...");
+    return callback(null, result);
+  }).catch(err => {
+    console.log("error");
+    console.log(err);
+    return callback(err, false);
+  });
+};
+
+exports.getSchedulesbyCinemaMovieDate = (cinema, movie, date, callback) => {
+  Models.Schedule.findAll({
+    subQuery: false,
+    where: {
+      [Op.and]: [{cinemaId: cinema}, {movieId: movie}, {screeningDate: date}]
+    },
+    include: [{
+      model: Models.Movie, as: 'movieIdSchedule'
+    }, {
+      model: Models.ShowRoom, as: 'cineIdRoomSche'
+    }]
+  }).then(result => {
+    console.log("getting schedules by cinema and movie...");
+    return callback(null, result);
+  }).catch(err => {
+    console.log("error");
+    console.log(err);
+    return callback(err, false);
+  });
+};
+
+exports.getTicketsTaken = (cinema, movie, date, time, callback) => {
+  Models.Ticket.findAll({
+    subQuery: false,
+    where: {
+      [Op.and]: [{cinemaId: cinema}, {movieId: movie}, {screeningDate: date}, {ticketTime: time}]
+    }
+  }).then(result => {
+    console.log("getting tickets...");
+    return callback(null, result);
+  }).catch(err => {
+    console.log("error");
+    console.log(err);
+    return callback(err, false);
+  });
+}
+
+// exports.addToTickets = (cinema, movie, date, time, seatNo, callback) => {
+//   Models.Ticket.create({
+//     cinemaId: cinema,
+//     movieId: movie,
+//     screeningDate: date,
+//     ticketTime: time,
+//     seatNumber: seatNo
+//   }).then(result => {
+//     console.log("getting tickets...");
+//     return callback(null, result);
+//   }).catch(err => {
+//     console.log("error");
+//     console.log(err);
+//     return callback(err, false);
+//   });
+// }
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ var db = require("./database");
 
 /* Main page */
 router.get("/", (req, res, next) => {
+  // console.log(req.query);
+  // req.query.id = 1;
   let validation = true;
   if (!req.query.id) {
     /* Check if the request has right parameter */
@@ -23,6 +25,135 @@ router.get("/", (req, res, next) => {
     });
   } else {
     /* TODO : Parameter Error Handleing */
+    next();
+  }
+});
+
+router.get("/book", (req, res, next) => {
+  if(Object.keys(req.query).length===0){
+    db.getSchedules((err, result) => {
+      if (err) {
+        next();
+      } else {
+        console.log("hello\n======================================");
+        res.json(result);
+        return;
+      }
+    });
+  }
+  else if(Object.keys(req.query).length===1){
+    if(req.query.movie){
+      db.getSchedulesbyMovie(req.query.movie, (err, result) => {
+        if (err) {
+          console.log(err);
+          next();
+        } else {
+          console.log("hello\n===========================");
+          res.json(result);
+          return;
+        }
+      });
+    } else if (req.query.cinema){
+      db.getSchedulesbyCinema(req.query.cinema, (err, result) => {
+        if (err) {
+          console.log(err);
+          next();
+        } else {
+          console.log("hello\n===========================");
+          res.json(result);
+          return;
+        }
+      });
+    } else {
+      db.getSchedulesbyDate(req.query.date, (err, result) => {
+        if (err) {
+          console.log(err);
+          next();
+        } else {
+          console.log("hello\n===========================");
+          res.json(result);
+          return;
+        }
+      });
+    }
+  }
+  else if (Object.keys(req.query).length===2) {
+    if(req.query.movie && req.query.cinema){
+      db.getSchedulesbyCinemaMovie(req.query.cinema, req.query.movie, (err, result) => {
+        if (err) {
+          console.log(err);
+          next();
+        } else {
+          console.log("hello\n===========================");
+          res.json(result);
+          return;
+        }
+      });
+    } else if(req.query.cinema && req.query.date){
+      db.getSchedulesbyCinemaDate(req.query.cinema, req.query.date, (err, result) => {
+        if (err) {
+          console.log(err);
+          next();
+        } else {
+          console.log("hello\n===========================");
+          res.json(result);
+          return;
+        }
+      });
+    } else if(req.query.movie && req.query.date){
+      db.getSchedulesbyMovieDate(req.query.movie, req.query.date, (err, result) => {
+        if (err) {
+          console.log(err);
+          next();
+        } else {
+          console.log("hello\n===========================");
+          res.json(result);
+          return;
+        }
+      });
+    }
+  } else {
+    db.getSchedulesbyCinemaMovieDate(req.query.cinema, req.query.movie, req.query.date, (err, result) => {
+      if (err) {
+        console.log(err);
+        next();
+      } else {
+        console.log("hello\n===========================");
+        res.json(result);
+        return;
+      }
+    });
+  }
+});
+
+get("/book/tickets", (req, res, next) => {
+  if (req.query.cinema && req.query.movie && req.query.date && req.query.time) {
+    db.getTicketsTaken(req.query.cinema, req.query.movie,
+                        req.query.date, req.query.time, (err, result) => {
+      if (err) {
+        next();
+      } else {
+        console.log("hello");
+        res.json(result);
+      }
+    });
+  } else {
+    next();
+  }
+});
+
+get("/book/tickets", (req, res, next) => {
+  if (req.query.cinema && req.query.movie && req.query.date && req.query.time && req.query.seatNo) {
+    db.confirmTickets(req.query.cinema, req.query.movie,
+                        req.query.date, req.query.time, req.query.seatNo, (err, result) => {
+      if (err) {
+        next();
+      } else {
+        console.log("hello");
+        res.json(result);
+      }
+    });
+  } else {
     next();
   }
 });
