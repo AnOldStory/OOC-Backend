381e3a15d73a7e8653ab7440160da0bec5071f3e
HyeongChang Lee <hc9904@hanyang.ac.kr>
2019-12-11 16:19:10 +0900

    Update public

---
+++ public/precache-manifest.207648a7f94e7d05bce6db72ee77c2aa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082258e07ea74ffd5eb11815f41f2080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "658680cefd43914c8ced",
+    "url": "/static/css/2.cbf2cf85.chunk.css"
+  },
+  {
+    "revision": "706dd1f39ff5167c7530",
+    "url": "/static/css/main.39889141.chunk.css"
+  },
+  {
+    "revision": "658680cefd43914c8ced",
+    "url": "/static/js/2.903a2af6.chunk.js"
+  },
+  {
+    "revision": "706dd1f39ff5167c7530",
+    "url": "/static/js/main.51cd7e19.chunk.js"
+  },
+  {
+    "revision": "5fa377074f5d55fe5b85",
+    "url": "/static/js/runtime-main.8a54bc64.js"
+  },
+  {
+    "revision": "3fa022346d53460c0a5b1596cee133aa",
+    "url": "/static/media/Logo.3fa02234.png"
+  },
+  {
+    "revision": "8456e908989ee5f586c598472da38860",
+    "url": "/static/media/avengers.8456e908.jpg"
+  },
+  {
+    "revision": "d3f5e8fa96213c47252bb25890ff6c1b",
+    "url": "/static/media/bird.d3f5e8fa.png"
+  },
+  {
+    "revision": "376b2f9adecc43cc5472c0f0e2735752",
+    "url": "/static/media/couple.376b2f9a.png"
+  },
+  {
+    "revision": "17dce1fa293cf31c96907c8240b297f1",
+    "url": "/static/media/jiyoungkim.17dce1fa.jpg"
+  }
+]);
